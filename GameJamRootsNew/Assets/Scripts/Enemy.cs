35d7c411258166106c6b0a94e97abf416ba89b37
Jamie <jaimesbond15@gmail.com>
2023-02-08 12:12:09 +0100

    [Added] - enemy state machine

--- GameJamRootsNew/Assets/Scripts/Enemy.cs
+++ GameJamRootsNew/Assets/Scripts/Enemy.cs
@@ -1,30 +1,84 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+
+public enum eState
+{
+    attack = 0,
+    stagger = 1
+}
 
 public class Enemy : MonoBehaviour
 {
+  
     public GameObject bulletPrefab;
+    public GameObject platform;
+    public int health = 10;
+    public int amountOfBulletNeeded = 2;
     public float shootingInterval = 10f;
-
+    public eState myState = eState.attack;
+    public float staggerDuration = 10f;
     private float lastShotTime;
+    private int amountOfHits = 0;
+    private float startStagger = 0;
     void Start()
     {
+        platform.SetActive(false);
         lastShotTime = Time.time;
+        startStagger = Time.time;
     }
 
+   void Shoot()
+    {
 
+        GameObject bullet = Instantiate(bulletPrefab, transform.position, transform.rotation);
+        bullet.transform.forward = transform.forward;
+    }
+
+    void OnHit() 
+    {
+        amountOfHits++;
+        print(amountOfHits);
+        if (amountOfHits == amountOfBulletNeeded)
+        {
+            amountOfHits = 0;
+            myState = eState.stagger;
+        }
+       
+    }
     // Update is called once per frame
     void Update()
     {
-        transform.LookAt(FindObjectOfType<Player>().transform);
-
-        if (Time.time - lastShotTime >= shootingInterval)
+        switch (myState) 
         {
-            GameObject bullet = Instantiate(bulletPrefab, transform.position, transform.rotation);
-            bullet.transform.forward = transform.forward;
-            lastShotTime = Time.time;
+            case eState.attack:
+
+                transform.LookAt(FindObjectOfType<Player>().transform);
+
+                if (Time.time - lastShotTime >= shootingInterval)
+                {
+                    Shoot();
+                    lastShotTime = Time.time;
+                }
+             
+                break;
+            case eState.stagger:
+
+                platform.SetActive(true);
+                if (Time.time - startStagger >= staggerDuration) 
+                {
+                    platform.SetActive(false);
+
+                    startStagger = Time.time;
+                    myState = eState.attack;
+
+
+                }
+                break;    
         }
+
+      
     }
 
 
